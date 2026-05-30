b7b354695dfbfe63bf5f54dd83a30a53751dcd6d
Philipp <56469697+Donnie-design@users.noreply.github.com>
2026-05-30 12:04:06 +0200

    ✅ Fix misconfiguration in tests

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
+    include: ['src/**/*.test.{ts,tsx}'],
     css: false,
     coverage: {
       provider: 'v8',
